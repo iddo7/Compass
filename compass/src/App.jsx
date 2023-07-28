82d47160168431c83be1f2fe8067517ba19b7ab1
iddo <isaacnegreiros10@gmail.com>
2023-07-28 15:03:02 -0400

    Install ReactBootstrap

--- compass/src/App.jsx
+++ compass/src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
 
 function App() {
@@ -11,6 +10,9 @@ function App() {
       <h1>
         Test
       </h1>
+      <button className='btn btn-dark'>
+        aaa
+      </button>
     </>
   )
 }
