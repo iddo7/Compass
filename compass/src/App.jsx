5039b936d7573439308e052b6c195d4810ed3386
iddo <isaacnegreiros10@gmail.com>
2023-07-28 15:54:38 -0400

    Adding: Profile Img, Username

--- compass/src/App.jsx
+++ compass/src/App.jsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css'
-import Header from './components/Header';
+import Header from './components/Header/Header';
 
 function App() {
 
