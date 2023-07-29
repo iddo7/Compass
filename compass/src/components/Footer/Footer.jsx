ffab8faeeab89cb06c6c9f37cb07dccc7fa6af49
iddo <isaacnegreiros10@gmail.com>
2023-07-29 15:06:40 -0400

    Centering footer without container fluid + centering icons

--- compass/src/components/Footer/Footer.jsx
+++ compass/src/components/Footer/Footer.jsx
@@ -12,26 +12,24 @@ const Footer = () => {
 
   return (
     <>
-        <div className="container-fluid d-flex justify-content-center">
-            <div id='iconBarContainer' className="container p-0 mx-auto">
-                <div className="row justify-content-center">
-                    <div id="iconBar" className={`col-${iconBarSize}`}>
-                        <div className="row">
-                            <div className="iconBox col-3">
-                                <FontAwesomeIcon icon={faSpotify} size={`${iconSize}x`}/>
-                            </div>
+        <div id='iconBarContainer' className="container p-0 mx-auto">
+            <div className="row justify-content-center">
+                <div id="iconBar" className={`col-${iconBarSize}`}>
+                    <div className="row">
+                        <div className="iconBox col-3">
+                            <FontAwesomeIcon icon={faSpotify} size={`${iconSize}x`}/>
+                        </div>
 
-                            <div className="iconBox col-3">
-                                <FontAwesomeIcon icon={faSoundcloud} size={`${iconSize}x`}/>
-                            </div>
+                        <div className="iconBox col-3">
+                            <FontAwesomeIcon icon={faSoundcloud} size={`${iconSize}x`}/>
+                        </div>
 
-                            <div className="iconBox col-3">
-                                <FontAwesomeIcon icon={faYoutube} size={`${iconSize}x`}/>
-                            </div>
+                        <div className="iconBox col-3">
+                            <FontAwesomeIcon icon={faYoutube} size={`${iconSize}x`}/>
+                        </div>
 
-                            <div className="iconBox col-3">
-                                <FontAwesomeIcon icon={faInstagram} size={`${iconSize}x`}/>
-                            </div>
+                        <div className="iconBox col-3">
+                            <FontAwesomeIcon icon={faInstagram} size={`${iconSize}x`}/>
                         </div>
                     </div>
                 </div>
