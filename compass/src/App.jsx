4d8297a28b76b425d017bf4470339f547d706b9a
iddo <isaacnegreiros10@gmail.com>
2023-09-02 12:36:08 -0400

    Resetting To no Carousel

--- compass/src/App.jsx
+++ compass/src/App.jsx
@@ -2,7 +2,6 @@ import { useState, useRef, useEffect } from "react"
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 import Header from './components/Header/Header'
-import Carousel from './components/Slider/Carousel'
 import Footer from './components/Footer/Footer'
 
 function App() {
@@ -12,7 +11,6 @@ function App() {
       <div className="container">
         <div className="row">
           <Header />
-          <Carousel />
           <Footer />
         </div>
       </div>
