8fe031db90646e2d04fd3d6dc53282b20de8fad8
iddo <isaacnegreiros10@gmail.com>
2023-09-05 18:48:12 -0400

    Adding animations to : quickLinks, card Images

--- compass/src/components/Carousel/Card/Card.jsx
+++ compass/src/components/Carousel/Card/Card.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
+import { motion } from 'framer-motion'
 import './Card.css'
-import cardImgUrl from "/assets/Maybe_Cover_V3-Smallest.jpg"
+import cardImgUrl from '/assets/Maybe_Cover_V3-Smallest.jpg'
 
 const Card = () => {
     return (
     <>
         <div className="card shadow">
-            <img src={cardImgUrl} alt={cardImgUrl} className="img-fluid shadow"/>
+            <motion.img src={cardImgUrl} alt={cardImgUrl} className="img-fluid shadow"
+                whileHover={{ scale : 1.1 }}
+                whileTap={{ scale : 0.95 }}/>
             <div className="info text-center">
                 <h3>Maybe</h3>
                 <p>pipoco, Yung Rider</p>
--- compass/src/components/Footer/QuickLink/QuickLink.jsx
+++ compass/src/components/Footer/QuickLink/QuickLink.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { fab } from '@fortawesome/free-brands-svg-icons'
+import { motion } from 'framer-motion'
 
 library.add(fab)
 
@@ -12,9 +13,12 @@ const QuickLink = ({ quickLink }) => {
     return (
         <>
             <a href={link} target='blank'>
-                <div className="iconBox col-3">
+                <motion.div className="iconBox col-3"
+                    whileHover={{ scale: 1.2 }}
+                    whileTap={{ scale: 0.95 }}
+                >
                     <FontAwesomeIcon icon={['fab', `${ icon }`]} size={`${iconSize}x`}/>
-                </div>
+                </motion.div>
             </a>
         </>
     )
